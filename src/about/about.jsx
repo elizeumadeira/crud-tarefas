82709ba1d007302a74d3f755f33c1b8e99bbfd3c
Elizeu Santos Madeira <elizeu.madeira@gmail.com>
2019-01-18 18:49:49 -0200

    Initial commit

---
+++ src/about/about.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PageHader from '../template/pageheader';
+
+export default props => (
+    <div>
+        <PageHader name="Sobre" small="Nós"></PageHader>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum</p>
+
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum</p>
+
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum</p>
+    </div>
+)
---
+++ src/template/pageheader.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default propst => (
+    <header className="page-header">
+        <h2>{propst.name} <small>{propst.small}</small></h2>
+    </header>
+)
